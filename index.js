98b8dc3f9d2f295bdb70cf3490bce25beb37bc35
LakiDev <77170483+LakiiDev@users.noreply.github.com>
2022-07-08 18:08:32 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+let showMenu=()=>
+{
+    let navLinks=document.querySelector(".links")
+    let open=document.querySelector(".menu")
+    let close=document.querySelector(".close")
+    let overlay=document.querySelector(".overlay")
+    let body=document.body
+    open.addEventListener("click",()=>
+    {
+        if(navLinks.style.display="none")
+        {
+            navLinks.style.setProperty("display","flex","important")
+            open.style.setProperty("display","none","important")
+            close.style.setProperty("display","flex","important")
+            overlay.style.setProperty("opacity","100%")
+            body.style.setProperty("overflow","hidden")
+        }
+        else
+        {
+            navLinks.style.setProperty("display","none","important")
+            open.style.setProperty("display","flex","important")
+            close.style.setProperty("display","none","important")
+        }
+    })
+    close.addEventListener("click",()=>
+    {
+        if(navLinks.style.display="flex")
+        {
+            navLinks.style.setProperty("display","none","important")
+            open.style.setProperty("display","flex","important")
+            close.style.setProperty("display","none","important")
+            overlay.style.setProperty("opacity","0%")
+            body.style.setProperty("overflow","visible")
+        }
+    })
+
+}
+showMenu()
